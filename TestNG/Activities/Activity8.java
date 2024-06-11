2ab792cb145ae6622c1912929149f6cd640eb823
chaiddya <chaiddya@in.ibm.com>
2024-06-11 14:29:08 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,24 @@
+package Activities;
+
+public class Activity8 {
+/*<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd" >
+<suite name="ExampleSuite" parallel="classes">
+    <test name="Parallel test">
+
+        <classes>
+            <class name="Activities.Activity1" />
+            <class name="Activities.Activity2" />
+            <class name="Activities.Activity3" />
+            <class name="Activities.Activity4_DemoOne" />
+            <class name="Activities.Activity4_DemoTwo" />
+            <class name="Activities.Activity5" />
+            <class name="Activities.Activity7" />
+
+
+        </classes>
+    </test>
+</suite>
+
+ */
+}
