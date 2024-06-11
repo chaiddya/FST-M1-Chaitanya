e5ddd793e8b3fbc8c9698745fb78da8b5a051da2
chaiddya <chaiddya@in.ibm.com>
2024-06-11 14:24:28 +0530

    Add files via upload

---
+++ JUnit/Activities/NotEnoughFund.java
@@ -0,0 +1,8 @@
+
+public class NotEnoughFund extends RuntimeException {
+
+    public NotEnoughFundsException(Integer amount, Integer balance) {
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+    }
+
+}
